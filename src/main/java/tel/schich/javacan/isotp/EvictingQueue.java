bfb89e93ad4bbbfcf06e72d0af1d5447ece38b27
Phillip Schichtel <phillip@schich.tel>
2018-09-03 00:10:22 +0200

    simplify and comment the EvictingQueue used for queue debugging

--- src/main/java/tel/schich/javacan/isotp/EvictingQueue.java
+++ src/main/java/tel/schich/javacan/isotp/EvictingQueue.java
@@ -1,26 +1,28 @@
 package tel.schich.javacan.isotp;
 
+/**
+ * This queue scales very badly increasing capacities. It is implemented that way to be easily viewable within a debugging
+ * view, as the last message is always in the last array position. Debugging is the <strong>only</strong> intended use-case
+ * for this queue!
+ * @param <T> The element type, should provide a good toString() implementation for debug purposes
+ */
 final class EvictingQueue<T> {
     private int i;
     private final T[] buf;
 
     @SuppressWarnings("unchecked")
     public EvictingQueue(int cap) {
+        if (cap == 0) {
+            throw new IllegalArgumentException("needs to handle at least one element!");
+        }
         buf = (T[]) new Object[cap];
     }
 
     public synchronized void offer(T f) {
-        if (i < buf.length) {
-            buf[i++] = f;
-        } else {
-            for (i = 1; i < buf.length; i++) {
-                buf[i - 1] = buf[i];
-            }
-            buf[buf.length - 1] = f;
+        i++;
+        for (i = 1; i < buf.length; i++) {
+            buf[i - 1] = buf[i];
         }
-    }
-
-    public synchronized int size() {
-        return i + 1;
+        buf[buf.length - 1] = f;
     }
 }
