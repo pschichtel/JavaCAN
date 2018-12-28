57d272425eda065e95a7cd310059692134a36330
Phillip Schichtel <phillip@schich.tel>
2018-12-28 02:09:46 +0100

    readd a similar API to to the old ISOTPBroker

---
+++ src/main/java/tel/schich/javacan/util/IsotpBroker.java
@@ -0,0 +1,154 @@
+/*
+ * The MIT License
+ * Copyright © 2018 Phillip Schichtel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package tel.schich.javacan.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.spi.AbstractSelector;
+import java.nio.channels.spi.SelectorProvider;
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadFactory;
+
+import tel.schich.javacan.IsotpCanChannel;
+
+public class IsotpBroker implements Closeable {
+    private final ByteBuffer readBuffer = ByteBuffer.allocateDirect(4096);
+    private final ThreadFactory threadFactory;
+    private final AbstractSelector selector;
+    private final Duration timeout;
+
+    private final Set<IsotpCanChannel> channels;
+    private final Map<IsotpCanChannel, MessageHandler> handlerMap;
+
+    private PollingThread poller;
+
+    public IsotpBroker(ThreadFactory threadFactory, SelectorProvider provider, Duration timeout) throws IOException {
+        this.threadFactory = threadFactory;
+        this.selector = provider.openSelector();
+        this.timeout = timeout;
+        this.channels = new HashSet<>();
+        this.handlerMap = new IdentityHashMap<>();
+    }
+
+    public void addChannel(IsotpCanChannel ch, MessageHandler handler) throws IOException {
+        if (ch.isRegistered()) {
+            throw new IllegalArgumentException("Channel already registered!");
+        }
+        ch.configureBlocking(false);
+        ch.register(this.selector, SelectionKey.OP_READ);
+        this.channels.add(ch);
+        this.handlerMap.put(ch, handler);
+        this.start();
+    }
+
+    public void start() {
+        if (poller != null) {
+            // already running
+            return;
+        }
+
+
+        this.poller = PollingThread.create("primary-poller", timeout.toMillis(), threadFactory, this::poll, this::handle);
+        this.poller.start();
+    }
+
+    private boolean poll(long timeout) {
+        if (channels.isEmpty()) {
+            try {
+                shutdown();
+            } catch (InterruptedException ignored) {}
+            return false;
+        }
+        try {
+            int n = selector.select(timeout);
+            if (n > 0) {
+                Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+                    SelectableChannel ch = key.channel();
+                    if (ch instanceof IsotpCanChannel) {
+                        IsotpCanChannel isotp = (IsotpCanChannel) ch;
+                        MessageHandler handler = handlerMap.get(ch);
+                        if (handler != null) {
+                            readBuffer.rewind();
+                            final int bytesRead = ((IsotpCanChannel) ch).read(readBuffer);
+                            readBuffer.rewind();
+                            handler.handle(isotp, readBuffer, 0, bytesRead);
+                        } else {
+                            System.err.println("Handler not found for channel: " + ch);
+                        }
+                    } else {
+                        System.err.println("Unsupported channel: " + ch);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        return true;
+    }
+
+    private boolean handle(Thread thread, Throwable t, boolean terminal) {
+        System.err.println("Polling thread failed: " + thread.getName());
+        t.printStackTrace(System.err);
+        System.err.println("Terminating other threads.");
+        try {
+            shutdown();
+        } catch (InterruptedException e) {
+            System.err.println("Got interrupted while stopping the threads");
+        }
+        return true;
+    }
+
+    public void shutdown() throws InterruptedException {
+        if (this.poller == null) {
+            // already stopped
+            return;
+        }
+        try {
+            this.poller.stop();
+            this.selector.wakeup();
+            this.poller.join();
+        } finally {
+            this.poller = null;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        try {
+            shutdown();
+        } catch (InterruptedException e) {
+            throw new IOException(e);
+        }
+    }
+}
---
+++ src/test/java/tel/schich/javacan/test/util/IsotpBrokerTest.java
@@ -0,0 +1,127 @@
+/*
+ * The MIT License
+ * Copyright © 2018 Phillip Schichtel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package tel.schich.javacan.test.util;
+
+import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import org.junit.jupiter.api.Test;
+
+import tel.schich.javacan.CanChannels;
+import tel.schich.javacan.IsotpCanChannel;
+import tel.schich.javacan.IsotpSocketAddress;
+import tel.schich.javacan.select.JavaCANSelectorProvider;
+import tel.schich.javacan.test.CanTestHelper;
+import tel.schich.javacan.util.IsotpBroker;
+import tel.schich.javacan.util.MessageHandler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsotpBrokerTest {
+
+    @Test
+    void testBroker() throws Exception {
+        ThreadFactory threadFactory = r -> {
+            Thread t = new Thread(r);
+            t.setName("broker-test-" + t.getName());
+            return t;
+        };
+
+        IsotpSocketAddress addra = IsotpSocketAddress.isotpAddress(0x7E0);
+        IsotpSocketAddress addrb = IsotpSocketAddress.isotpAddress(0x7E8);
+
+        try (IsotpBroker broker = new IsotpBroker(threadFactory, new JavaCANSelectorProvider(), Duration.ofSeconds(5))) {
+
+            try (IsotpCanChannel a = CanChannels.newIsotpChannel()) {
+                try (IsotpCanChannel b = CanChannels.newIsotpChannel()) {
+                    final Lock lock = new ReentrantLock();
+                    final Condition condition = lock.newCondition();
+
+                    a.bind(CanTestHelper.CAN_INTERFACE, addra, addrb);
+                    b.bind(CanTestHelper.CAN_INTERFACE, addrb, addra);
+
+                    broker.addChannel(a, new PingPing(lock, condition));
+                    broker.addChannel(b, new PingPing(lock, condition));
+
+                    ByteBuffer buf = ByteBuffer.allocateDirect(1);
+                    buf.put(0, (byte)1);
+                    a.write(buf);
+
+                    try {
+                        lock.lock();
+                        assertTrue(condition.await(20, TimeUnit.SECONDS), "The backing CAN socket should be fast enough to reach 4096 bytes within 20 seconds of ping-pong");
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+    }
+
+    public static String hexDump(ByteBuffer data, int offset, int length) {
+        StringBuilder s = new StringBuilder(length * 2);
+        if (length > 0) {
+            s.append(String.format("%02X", data.get(offset)));
+            for (int i = 1; i < length; ++i) {
+                s.append('.').append(String.format("%02X", data.get(offset + i)));
+            }
+        }
+        return s.toString();
+    }
+
+    private static final class PingPing implements MessageHandler {
+        private final Lock lock;
+        private final Condition condition;
+
+        public PingPing(Lock lock, Condition condition) {
+            this.lock = lock;
+            this.condition = condition;
+        }
+
+        @Override
+        public void handle(IsotpCanChannel ch, ByteBuffer payload, int offset, int length) {
+            /*if (length % 200 == 0) */{
+                System.out.println(String.format("(%04d) -> %08X#%s", length, ch.getTxAddress().getId(), hexDump(payload, offset, length)));
+                System.out.flush();
+            }
+            payload.put(length, (byte)(Math.random() * 255));
+            try {
+                ch.write(payload, 0, length + 1);
+            } catch (Exception e) {
+                assertEquals(IllegalArgumentException.class, e.getClass());
+                System.err.println("Failed to send message:");
+                e.printStackTrace(System.err);
+                lock.lock();
+                try {
+                    condition.signalAll();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+}
