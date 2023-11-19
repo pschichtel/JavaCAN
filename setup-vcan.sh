66c3ef0a1ebbdb38b8338507d797caa0cd801787
Phillip Schichtel <phillip@schich.tel>
2023-11-19 19:29:44 +0100

    add more modules explicitly and run in strict mode

--- setup-vcan.sh
+++ setup-vcan.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+set -eup pipefail
+
+sudo modprobe vcan can_raw can_bcm
+
 interface_name="${1:-vcan0}"
 
 sudo ip link add dev "$interface_name" type vcan
 sudo ip link set up "$interface_name"
+
 if ! sudo modprobe can_isotp
 then
     echo "Failed to load the ISOTP module, related unit tests will probably fail"
