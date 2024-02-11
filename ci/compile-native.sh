ae2aef4966423338d60e7f26f0370a6cd0d9d621
Phillip Schichtel <phillip@schich.tel>
2024-02-11 01:59:01 +0100

    correctly locate the actual script

--- ci/compile-native.sh
+++ ci/compile-native.sh
@@ -2,5 +2,6 @@
 
 set -euo pipefail
 
+here="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 dockcross_image="${4?no arch given}"
-./compile-native.sh "$@" && podman image rm "$dockcross_image"
+"$here/../compile-native.sh" "$@" && podman image rm "$dockcross_image"
