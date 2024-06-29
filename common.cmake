10adff3c2c809f633511cc4f6061d9dd8f2f4f15
Phillip Schichtel <phillip@schich.tel>
2024-06-29 14:57:37 +0200

    re-add static linking and remove the maven reference

--- common.cmake
+++ common.cmake
@@ -1,7 +1,8 @@
 set(CMAKE_C_STANDARD 99)
 
 option(IS_RELEASE "Whether this is a release build" OFF)
-option(MAVEN_VERSION "The version of the maven project")
+option(LINK_STATICALLY "Whether this is a release build" OFF)
+option(PROJECT_VERSION "The version of the maven project")
 
 find_package(Java 1.8 REQUIRED)
 
@@ -11,7 +12,7 @@ include_directories(
         "$ENV{JAVA_HOME}/include/linux"
         build/jni)
 
-add_compile_options(-Werror -fPIC -D "MVN_VERSION=${MAVEN_VERSION}")
+add_compile_options(-Werror -fPIC -D "PROJECT_VERSION=${PROJECT_VERSION}")
 if(IS_RELEASE)
     add_compile_options(-O2 -flto)
 else()
@@ -19,3 +20,6 @@ else()
 endif()
 
 add_link_options(-z noexecstack -fvisibility=hidden)
+if(LINK_STATICALLY)
+    add_link_options(-static)
+endif()
