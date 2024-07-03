bff643997bd714d40851502ef6953ec2af8f086d
Phillip Schichtel <phillip@schich.tel>
2024-07-03 20:07:45 +0200

    the ci script is also obsolete

--- ci/compile-native.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-here="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-dockcross_image="${4?no arch given}"
-"$here/../compile-native.sh" "$@" && podman image rm "$dockcross_image"
