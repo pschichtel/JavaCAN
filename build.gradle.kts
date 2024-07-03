5eff708fd7dfd21cc9762b0491d3cc36e9a54a30
Phillip Schichtel <phillip@schich.tel>
2024-07-03 21:06:10 +0200

    derive the version from git

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,24 @@
 plugins {
     `maven-publish`
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("pl.allegro.tech.build.axion-release") version "1.16.1" // this is the last version that supports Java 8
 }
+
+scmVersion {
+    tag {
+        prefix = "javacan-"
+    }
+    nextVersion {
+        suffix = "SNAPSHOT"
+        separator = "-"
+    }
+}
+
+val gitVersion: String = scmVersion.version
 
 allprojects {
     group = "tel.schich"
-    version = "3.5.0-SNAPSHOT"
+    version = gitVersion
 }
 
 nexusPublishing {
