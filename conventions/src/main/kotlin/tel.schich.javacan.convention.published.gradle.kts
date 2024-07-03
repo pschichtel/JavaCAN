b7c52450dd503e97654774fc6e68e50fee76e840
Phillip Schichtel <phillip@schich.tel>
2024-07-03 02:23:59 +0200

    properly configure publishing

--- conventions/src/main/kotlin/tel.schich.javacan.convention.published.gradle.kts
+++ conventions/src/main/kotlin/tel.schich.javacan.convention.published.gradle.kts
@@ -4,31 +4,45 @@ plugins {
     `maven-publish`
 }
 
-publishing.publications.register(project.name, MavenPublication::class) {
-    artifactId = "javacan-${project.name}"
-    from(components["java"])
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
 
-    pom {
-        name = "JavaCAN"
-        description = project.description
-        url = "https://github.com/pschichtel/JavaCAN"
-        licenses {
-            license {
-                name = "MIT"
-                url = "https://opensource.org/licenses/MIT"
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            artifactId = "javacan-${project.name}"
+            from(components["java"])
+
+            pom {
+                name = "JavaCAN"
+                description = project.description
+                url = "https://github.com/pschichtel/JavaCAN"
+                licenses {
+                    license {
+                        name = "MIT"
+                        url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("pschichtel")
+                        name.set("Phillip Schichtel")
+                        email.set("phillip@schich.tel")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/pschichtel/JavaCAN")
+                    connection.set("scm:git:https://github.com/pschichtel/JavaCAN")
+                    developerConnection.set("scm:git:git@github.com:pschichtel/JavaCAN")
+                }
             }
-        }
-        developers {
-            developer {
-                id.set("pschichtel")
-                name.set("Phillip Schichtel")
-                email.set("phillip@schich.tel")
-            }
-        }
-        scm {
-            url.set("https://github.com/pschichtel/JavaCAN")
-            connection.set("scm:git:https://github.com/pschichtel/JavaCAN")
-            developerConnection.set("scm:git:git@github.com:pschichtel/JavaCAN")
         }
     }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
