615fcf8508b3f1492c2e4e5ff1f31496b960a0ed
Phillip Schichtel <phillip@schich.tel>
2024-07-21 19:11:01 +0200

    upgrade the dockcross plugin to support compiling on windows

--- conventions/build.gradle.kts
+++ conventions/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    api(plugin("tel.schich.dockcross", version = "0.1.1"))
+    api(plugin("tel.schich.dockcross", version = "0.2.3"))
 }
 
 fun plugin(id: String, version: String) = "$id:$id.gradle.plugin:$version"
