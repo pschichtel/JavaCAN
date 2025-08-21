55295998940016d3ba96fcac85e0af7fc087c859
pschichbot <phillip.github-bot@schich.tel>
2025-08-21 23:04:39 +0000

    Update all dependencies

--- conventions/src/main/kotlin/tel.schich.javacan.convention.native.gradle.kts
+++ conventions/src/main/kotlin/tel.schich.javacan.convention.native.gradle.kts
@@ -31,7 +31,7 @@ data class BuildTarget(
     val archDetect: Boolean,
 )
 
-val dockcrossVersion = "20250109-7bf589c"
+val dockcrossVersion = "20250818-0a27819"
 val nativeGroup = "native"
 val targets = buildList {
     fun MutableList<BuildTarget>.add(
