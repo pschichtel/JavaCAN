ea418cfe22598a2e03414bf15c335384b54be68d
pschichbot <phillip.github-bot@schich.tel>
2026-05-24 02:05:01 +0000

    Update plugin dev.panuszewski.typesafe-conventions to v0.11.1

--- conventions/settings.gradle.kts
+++ conventions/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.panuszewski.typesafe-conventions") version "0.11.0"
+    id("dev.panuszewski.typesafe-conventions") version "0.11.1"
 }
 
 dependencyResolutionManagement {
